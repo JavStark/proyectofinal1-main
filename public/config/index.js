0aef280287368a4fc40b09566da93cecb9fffd42
JavStark <rjaviersosa@gmail.com>
2022-04-06 17:56:36 -0300

    Proyecto Final-Part. 1

---
+++ public/config/index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+let config = {
+  
+  port:process.env.PORT || '3000', // access with process.env to PORT variable in .env module
+  cors:process.env.CORS || '*',
+  email_support:process.env.EMAIL_SUPPORT || '',
+  email_error:process.env.EMAIL_ERROR || '',
+  node_env:process.env.NODE_ENV !=='production',
+
+}
+
+module.exports = { config }
+
